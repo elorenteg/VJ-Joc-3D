a9ac217d89e948a19177551cf780663482bf6b0a
Ester <ester.lorente@outlook.com>
2016-04-17 17:29:56 +0200

    MainMenu loads scene and exit

--- Assets/Scripts/MenuKeyboard.cs
+++ Assets/Scripts/MenuKeyboard.cs
@@ -5,8 +5,15 @@ using System.Collections;
 public class MenuKeyboard : MonoBehaviour {
 
     private string[] mainMenuLabels = {"Jugar", "Instruccions", "Opcions", "Crèdits", "Sortir"};
-    private bool[] mainMenuButtons;
+    //private bool[] mainMenuButtons;
+    private const int JUGAR = 0;
+    private const int INSTR = 1;
+    private const int OPTNS = 2;
+    private const int CREDS = 3;
+    private const int SORTR = 4;
+
     private int mainMenuSelected;
+    private int mainMenuAction;
 
     private GUIStyle normalFont;
     private GUIStyle selectFont;
@@ -19,9 +26,10 @@ public class MenuKeyboard : MonoBehaviour {
     public AudioClip moveSound;
 
     void Start() {
+        mainMenuAction = -1;
         mainMenuSelected = 0;
-        mainMenuButtons = new bool[mainMenuLabels.Length];
-        mainMenuButtons[mainMenuSelected] = true;
+        //mainMenuButtons = new bool[mainMenuLabels.Length];
+        //mainMenuButtons[mainMenuSelected] = true;
 
         normalFont = new GUIStyle(); normalFont.fontSize = 28;
         selectFont = new GUIStyle(); selectFont.fontSize = 32;
@@ -36,26 +44,26 @@ public class MenuKeyboard : MonoBehaviour {
     void Update() {
         if (Input.GetKeyDown(KeyCode.DownArrow) == true) {
             AudioSource.PlayClipAtPoint(moveSound, transform.position);
-            mainMenuButtons[mainMenuSelected] = false;
+            //mainMenuButtons[mainMenuSelected] = false;
 
             mainMenuSelected++;
             mainMenuSelected = Mathf.Min(mainMenuSelected, mainMenuLabels.Length-1);
             
-            mainMenuButtons[mainMenuSelected] = true;
+            //mainMenuButtons[mainMenuSelected] = true;
         }
 
         if (Input.GetKeyDown(KeyCode.UpArrow) == true) {
             AudioSource.PlayClipAtPoint(moveSound, transform.position);
-            mainMenuButtons[mainMenuSelected] = false;
+            //mainMenuButtons[mainMenuSelected] = false;
 
             mainMenuSelected--;
             mainMenuSelected = Mathf.Max(mainMenuSelected, 0);
             
-            mainMenuButtons[mainMenuSelected] = true;
+            //mainMenuButtons[mainMenuSelected] = true;
         }
 
         if (Input.GetKeyDown(KeyCode.Return) == true) {
-            if (mainMenuSelected == 0) SceneManager.LoadScene("Level2");
+            mainMenuAction = mainMenuSelected;
         }
     }
 
@@ -82,28 +90,27 @@ public class MenuKeyboard : MonoBehaviour {
         Graphics.DrawTexture(new Rect(Screen.width / 2 - w_sep / 2, yo_text-h_sep, w_sep, h_sep), separatorTexture);
         Graphics.DrawTexture(new Rect(Screen.width / 2 - w_sep / 2, yo_text+mainMenuLabels.Length*50 -20, w_sep, h_sep), separatorTexture);
         for (int i = 0; i < mainMenuLabels.Length; i++) {
-            if (mainMenuButtons[i]) {
+            if (mainMenuSelected == i) {
                 GUI.Button(new Rect(xo_text, yo_text + i * 50, w_text, h_text), mainMenuLabels[i], selectFont);
                 Graphics.DrawTexture(new Rect(Screen.width / 2 - w_sel / 2, yo_text - 10 + i * 50, w_sel, h_sel), selectTexture);
             }
             else GUI.Button(new Rect(xo_text, yo_text + i * 50, w_text, h_text), mainMenuLabels[i], normalFont);
         }
 
-        if (mainMenuButtons[0]) { }
-
-        if (mainMenuButtons[1]) { }
-
-        if (mainMenuButtons[2]) { }
-
-        if (mainMenuButtons[3]) { }
-
-        if (mainMenuButtons[4]) {
-            Debug.Log("4");
-            Application.Quit();
-        }
-
-        if (Input.GetKey(KeyCode.Return)) {
-            mainMenuButtons[mainMenuSelected] = true;
+        switch(mainMenuAction)
+        {
+            case JUGAR:
+                SceneManager.LoadScene("Level2");
+                break;
+            case INSTR:
+                break;
+            case OPTNS:
+                break;
+            case CREDS:
+                break;
+            case SORTR:
+                Application.Quit();
+                break;
         }
     }
 }
