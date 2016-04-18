40413a1706fb89d2857b46ee5f5157cfca918d20
Ester <ester.lorente@outlook.com>
2016-04-18 02:01:09 +0200

    Translate & Rotate Pacman not working

--- Assets/Scripts/PacmanMove.cs
+++ Assets/Scripts/PacmanMove.cs
@@ -1,45 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PacmanMove : MonoBehaviour {
-    private int state;
-    private int MAX_STATE = 5;
+public class PacmanMove : MonoBehaviour
+{
+    public float moveSpeed;
+    public float turnSpeed;
 
-	// Use this for initialization
-	void Start () {
-        state = 0;
+    // Use this for initialization
+    void Start () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float posX, posY, posZ;
-        posX = 0.0f;
-        posY = 0.0f;
-        posZ = 0.0f;
-
-        // Input
         bool move = false;
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
-            posX = -1.0f;
-            move = true;
+            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
-            posX = 1.0f;
-            move = true;
+            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) {
-            posZ = 1.0f;
-            move = true;
-        }
-        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) {
-            posZ = -1.0f;
-            move = true;
-        }
-
-        if (move) {
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
             GetComponent<Animation>().Play("ArmatureActionPlayer", PlayMode.StopAll);
-            state = (state++) % MAX_STATE;
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
         }
-        transform.Translate(posX, posY, posZ);
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            GetComponent<Animation>().Play("ArmatureActionPlayer", PlayMode.StopAll);
+            transform.Translate(-Vector3.left * moveSpeed * Time.deltaTime);
+        }
     }
 }
