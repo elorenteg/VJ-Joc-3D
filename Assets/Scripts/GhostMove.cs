b057245b0888b084346be4c8c4bb472e9306b9be
Ester <ester.lorente@outlook.com>
2016-04-29 09:25:01 +0200

    Ghost Animation in another script

--- Assets/Scripts/GhostMove.cs
+++ Assets/Scripts/GhostMove.cs
@@ -16,23 +16,7 @@ public class GhostMove : MonoBehaviour
     private int MAX_TIME_STATE;
 
     private SkinnedMeshRenderer skinnedMeshRenderer;
-    private const int BODY = 0;
-    private const int EYES = 1;
-    private const int HALO_CEN = 3;
-    private const int HALO_END = 2;
-
-    private const int TRANSPARENT = 0;
-    private const int CUTOUT = 1;
-
-    public Texture bodyNormalTex;
-    public Texture eyesNormalTex;
-    public Texture haloCenterNormalTex;
-    public Texture haloEndNormalTex;
-
-    public Texture bodyDeadTex;
-    public Texture eyesDeadTex;
-    public Texture haloCenterDeadTex;
-    public Texture haloEndDeadTex;
+    private GhostAnimate animationScript;
 
     // Use this for initialization
     void Start ()
@@ -43,102 +27,22 @@ public class GhostMove : MonoBehaviour
         MAX_TIME_STATE = 15;
 
         skinnedMeshRenderer = GetComponentInChildren<SkinnedMeshRenderer>();
-        UpdateTextures();
+        animationScript = GetComponent<GhostAnimate>();
+        animationScript.SetTextures(animationScript.stateMove(), state);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //GetComponent<Animation>().Play("Die", PlayMode.StopAll);
-
         if (timeState == MAX_TIME_STATE)
         {
             timeState = 0;
             state = state + 1;
             if (state == 2) state = 0;
+
+            animationScript.SetTextures(animationScript.stateMove(), state);
         }
 
         ++timeState;
-
-        UpdateCoordinates();
-    }
-
-    void UpdateTextures()
-    {
-        if (!isDead)
-        {
-            SetupMaterialWithBlendMode(skinnedMeshRenderer.materials[BODY], CUTOUT);
-            skinnedMeshRenderer.materials[BODY].mainTexture = bodyNormalTex;
-            skinnedMeshRenderer.materials[EYES].mainTexture = eyesNormalTex;
-            skinnedMeshRenderer.materials[HALO_CEN].mainTexture = haloCenterNormalTex;
-            skinnedMeshRenderer.materials[HALO_END].mainTexture = haloEndNormalTex;
-        }
-        else
-        {
-            SetupMaterialWithBlendMode(skinnedMeshRenderer.materials[BODY], TRANSPARENT);
-            skinnedMeshRenderer.materials[BODY].mainTexture = bodyDeadTex;
-            skinnedMeshRenderer.materials[EYES].mainTexture = eyesDeadTex;
-            skinnedMeshRenderer.materials[HALO_CEN].mainTexture = haloCenterDeadTex;
-            skinnedMeshRenderer.materials[HALO_END].mainTexture = haloEndDeadTex;
-        }
-
-        UpdateCoordinates();
-    }
-
-    void UpdateCoordinates()
-    {
-        if (!isDead)
-        {
-            skinnedMeshRenderer.materials[BODY].mainTextureScale = new Vector2(1.0f, 1.0f);
-            skinnedMeshRenderer.materials[BODY].mainTextureOffset = new Vector2(0.0f, 0.0f);
-        }
-        else
-        {
-            skinnedMeshRenderer.materials[BODY].mainTextureScale = new Vector2(0.5f, 1.0f);
-            if (state == 0)
-                skinnedMeshRenderer.materials[BODY].mainTextureOffset = new Vector2(0.0f, 0.0f);
-            else
-                skinnedMeshRenderer.materials[BODY].mainTextureOffset = new Vector2(0.5f, 0.0f);
-        }
-
-        if (state == 0)
-        {
-            Vector2 offset = new Vector2(0.0f, 0.0f);
-            skinnedMeshRenderer.materials[EYES].mainTextureOffset = offset;
-            skinnedMeshRenderer.materials[HALO_CEN].mainTextureOffset = offset;
-            skinnedMeshRenderer.materials[HALO_END].mainTextureOffset = offset;
-        }
-        else
-        {
-            Vector2 offset = new Vector2(0.5f, 0.0f);
-            skinnedMeshRenderer.materials[EYES].mainTextureOffset = offset;
-            skinnedMeshRenderer.materials[HALO_CEN].mainTextureOffset = offset;
-            skinnedMeshRenderer.materials[HALO_END].mainTextureOffset = offset;
-        }
-    }
-
-    public static void SetupMaterialWithBlendMode(Material material, int blendMode)
-    {
-        switch (blendMode)
-        {
-            case CUTOUT:
-                material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
-                material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
-                material.SetInt("_ZWrite", 1);
-                material.EnableKeyword("_ALPHATEST_ON");
-                material.DisableKeyword("_ALPHABLEND_ON");
-                material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-                material.renderQueue = 2450;
-                break;
-            case TRANSPARENT:
-                material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
-                material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-                material.SetInt("_ZWrite", 0);
-                material.DisableKeyword("_ALPHATEST_ON");
-                material.DisableKeyword("_ALPHABLEND_ON");
-                material.EnableKeyword("_ALPHAPREMULTIPLY_ON");
-                material.renderQueue = 3000;
-                break;
-        }
     }
 }
