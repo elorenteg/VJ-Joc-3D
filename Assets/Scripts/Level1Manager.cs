d32550c621343b37bb3b644839daa3d575e2cab4
Marc <marcvila10@hotmail.com>
2016-04-29 09:38:50 +0200

    First Ghost instantiated in Script

--- Assets/Scripts/Level1Manager.cs
+++ Assets/Scripts/Level1Manager.cs
@@ -9,6 +9,7 @@ public class Level1Manager : MonoBehaviour
     public GameObject cube;
     public GameObject floor;
     public Texture wallTexture;
+    public GameObject ghost;
 
     public static string appPath = "..\\VJ-Joc-3D";
 
@@ -19,20 +20,32 @@ public class Level1Manager : MonoBehaviour
     private int MAP_HEIGHT;
 
     private float WALL_HEIGHT = 7.5f;
+    private float GHOST_Y_POS = 19.0f;
 
     private static int CELL_EMPTY = 0;
     private static char WALL_V = 'V';
     private static int WALL_V_C = 1;
     private static char WALL_H = 'H';
     private static int WALL_H_C = 2;
+    private static char GHOST_B = 'B';
+    private static int GHOST_B_C = 3;
+    private static char GHOST_O = 'O';
+    private static int GHOST_O_C = 4;
+    private static char GHOST_P = 'B';
+    private static int GHOST_P_C = 5;
+    private static char GHOST_R = 'B';
+    private static int GHOST_R_C = 6;
 
     private const int TILE_SIZE = 2;
+
+    private Vector3 GHOST_SCALE = new Vector3(6.0f, 6.0f, 6.0f);
+    private Vector2 GHOST_TEXTURE_SCALE = new Vector2(0.5f, 1.0f);
 
     void Start()
     {
         readMap();
         placeFloor();
-        placeWalls();
+        placeObjects();
     }
 
     void Update()
@@ -76,6 +89,22 @@ public class Level1Manager : MonoBehaviour
                             {
                                 MapLine[j] = WALL_H_C;
                             }
+                            else if (line[j] == GHOST_B)
+                            {
+                                MapLine[j] = GHOST_B_C;
+                            }
+                            else if (line[j] == GHOST_O)
+                            {
+                                MapLine[j] = GHOST_O_C;
+                            }
+                            else if (line[j] == GHOST_P)
+                            {
+                                MapLine[j] = GHOST_P_C;
+                            }
+                            else if (line[j] == GHOST_R)
+                            {
+                                MapLine[j] = GHOST_R_C;
+                            }
                             else
                             {
                                 MapLine[j] = CELL_EMPTY;
@@ -107,7 +136,7 @@ public class Level1Manager : MonoBehaviour
         newFloor.transform.localScale = floorScale;
     }
 
-    void placeWalls()
+    void placeObjects()
     {
         for (int i = 0; i < MAP_HEIGHT; ++i)
         {
@@ -116,6 +145,7 @@ public class Level1Manager : MonoBehaviour
                 int cell = Map[i][j];
                 if (cell != CELL_EMPTY)
                 {
+                    GameObject element;
                     Vector3 cellPosition;
                     Vector3 cellScale;
                     Texture texture;
@@ -124,7 +154,8 @@ public class Level1Manager : MonoBehaviour
 
                     if (cell == WALL_V_C)
                     {
-                        cellPosition = new Vector3(j * TILE_SIZE, WALL_HEIGHT/2, i * TILE_SIZE);
+                        element = cube;
+                        cellPosition = new Vector3(j * TILE_SIZE, WALL_HEIGHT / 2, i * TILE_SIZE);
                         cellScale = new Vector3(2.0f, WALL_HEIGHT, 2.5f);
                         texture = wallTexture;
                         textureScale = new Vector2(0.5f, 1.0f);
@@ -133,25 +164,37 @@ public class Level1Manager : MonoBehaviour
                     }
                     else if (cell == WALL_H_C)
                     {
-                        cellPosition = new Vector3(j * TILE_SIZE, WALL_HEIGHT/2, i * TILE_SIZE);
+                        element = cube;
+                        cellPosition = new Vector3(j * TILE_SIZE, WALL_HEIGHT / 2, i * TILE_SIZE);
                         cellScale = new Vector3(2.0f, WALL_HEIGHT, 2.5f);
                         texture = wallTexture;
                         textureScale = new Vector2(0.5f, 1.0f);
 
                         cellQuaternion = Quaternion.AngleAxis(0.0f, Vector3.up);
                     }
-                    else
+                    else if (cell == GHOST_B_C)
                     {
-                        Debug.LogError("Creating a non empty cell");
-                        cellPosition = new Vector3(0, 0, 0);
-                        cellScale = new Vector3(0.0f, 0.0f, 0.0f);
-                        texture = wallTexture;
-                        textureScale = new Vector2(0.0f, 0.0f);
+                        element = ghost;
+                        cellPosition = new Vector3(j * TILE_SIZE, GHOST_Y_POS, i * TILE_SIZE);
+                        cellScale = new Vector3(GHOST_SCALE.x, GHOST_SCALE.y, GHOST_SCALE.z);
+                        texture = null;
+                        textureScale = new Vector2(GHOST_TEXTURE_SCALE.x, GHOST_TEXTURE_SCALE.y);
 
                         cellQuaternion = Quaternion.AngleAxis(0.0f, Vector3.up);
                     }
+                    else
+                    {
+                        Debug.LogError("Creating a non empty cell");
+                        element = null;
+                        cellPosition = Vector3.zero;
+                        cellScale = Vector3.zero;
+                        texture = null;
+                        textureScale = Vector2.zero;
 
-                    GameObject newObject = Instantiate(cube, cellPosition, cube.transform.rotation) as GameObject;
+                        cellQuaternion = Quaternion.AngleAxis(0.0f, Vector3.zero);
+                    }
+
+                    GameObject newObject = Instantiate(element, cellPosition, element.transform.rotation) as GameObject;
                     newObject.transform.parent = transform;
                     newObject.transform.localScale = cellScale;
 
