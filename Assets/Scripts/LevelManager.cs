d410971e2fa3b590b0bae436c2a270bc1c5abd78
Marc <marcvila10@hotmail.com>
2016-05-12 20:41:44 +0200

    HighScore working

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -12,9 +12,11 @@ public class LevelManager : MonoBehaviour
     private static int INITIAL_LIFES = 3;
     private static string LEVEL_LABEL = "LEVEL";
     private static string SCORE_LABEL = "SCORE";
+    private static string MAX_SCORE_LABEL = "HIGHSCORE";
     private static string LIFES_LABEL = "LIFES";
     private int currentLevel;
     private int currentScore;
+    private int currentHighScore;
     private int currentLifes;
     private int remainingCoins;
 
@@ -23,15 +25,18 @@ public class LevelManager : MonoBehaviour
     private static int COIN_SCORE = 1;
 
     private GUIStyle normalFont;
+    private Texture2D backgroundLevelTexture;
     private Texture2D backgroundScoreTexture;
     private Texture2D backgroundLifesTexture;
 
     private LevelCreator levelCreator;
+    private DataManager dataManager;
 
     void Start()
     {
         GameObject gameManager = GameObject.Find("GameManager");
         levelCreator = gameManager.GetComponent<LevelCreator>();
+        dataManager = gameManager.GetComponent<DataManager>();
 
         normalFont = new GUIStyle();
         normalFont.font = (Font)Resources.Load("Fonts/lilliput steps", typeof(Font));
@@ -40,7 +45,10 @@ public class LevelManager : MonoBehaviour
         normalFont.normal.textColor = Color.green;
 
         backgroundScoreTexture = Resources.Load(texturesPath + "score_tube") as Texture2D;
+        backgroundLevelTexture = backgroundScoreTexture;
         backgroundLifesTexture = backgroundScoreTexture;
+
+        currentHighScore = dataManager.readMaxScore();
 
         startGame();
     }
@@ -71,7 +79,7 @@ public class LevelManager : MonoBehaviour
     void OnGUI()
     {
         // Score bg
-        GUI.DrawTexture(new Rect(5, 5, 130, 50), backgroundScoreTexture);
+        GUI.DrawTexture(new Rect(5, 5, 135, 50), backgroundScoreTexture);
 
         // Score label
         GUI.Label(new Rect(-80, 50, 200, 20), SCORE_LABEL, normalFont);
@@ -79,23 +87,32 @@ public class LevelManager : MonoBehaviour
         // Score value
         GUI.Label(new Rect(-95, 11, 200, 20), currentScore.ToString(), normalFont);
 
+        // Max. Score bg
+        GUI.DrawTexture(new Rect(Screen.width - 185, 5, 180, 50), backgroundScoreTexture);
+
+        // Max. Score label
+        GUI.Label(new Rect(Screen.width - 205, 50, 200, 20), MAX_SCORE_LABEL, normalFont);
+
+        // Max. Score value
+        GUI.Label(new Rect(Screen.width - 235, 11, 200, 20), currentHighScore.ToString(), normalFont);
+
         // Life bg
-        GUI.DrawTexture(new Rect(Screen.width - 135, 5, 130, 50), backgroundLifesTexture);
+        GUI.DrawTexture(new Rect(5, Screen.height - 55, 135, 50), backgroundLifesTexture);
 
         // Life label
-        GUI.Label(new Rect(Screen.width - 220, 50, 200, 20), LIFES_LABEL, normalFont);
+        GUI.Label(new Rect(-80, Screen.height - 90, 200, 20), LIFES_LABEL, normalFont);
 
         // Life value
-        GUI.Label(new Rect(Screen.width - 255, 11, 200, 20), currentLifes.ToString(), normalFont);
+        GUI.Label(new Rect(-95, Screen.height - 50, 200, 20), currentLifes.ToString(), normalFont);
 
         // Level bg
-        GUI.DrawTexture(new Rect(0, Screen.height - 55, 130, 50), backgroundLifesTexture);
+        GUI.DrawTexture(new Rect(Screen.width - 140, Screen.height - 55, 135, 50), backgroundLevelTexture);
 
         // Level label
-        GUI.Label(new Rect(-80, Screen.height - 90, 200, 20), LEVEL_LABEL, normalFont);
+        GUI.Label(new Rect(Screen.width - 215, Screen.height - 90, 200, 20), LEVEL_LABEL, normalFont);
 
         // Level value
-        GUI.Label(new Rect(-95, Screen.height - 50, 200, 20), currentLevel.ToString(), normalFont);
+        GUI.Label(new Rect(Screen.width - 235, Screen.height - 50, 200, 20), currentLevel.ToString(), normalFont);
     }
 
     public void coinEaten()
@@ -125,6 +142,11 @@ public class LevelManager : MonoBehaviour
     private void increaseScore(int quantity)
     {
         currentScore += quantity;
+        if (currentScore > currentHighScore)
+        {
+            currentHighScore = currentScore;
+            dataManager.saveMaxScore(currentScore);
+        }
     }
 
     public int getScore()
